c2028dab4c9d85ded9bbdfe8321ac2d12254ff80
adna olívia <88201392+adnaolivia@users.noreply.github.com>
2023-02-13 22:30:25 -0300

    Update zonkos_main.java

--- zonkos_main.java
+++ zonkos_main.java
@@ -1,113 +1,114 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+              
 public class Main {
-  public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-
-    List<Usuario> listaUsuarios = new ArrayList<Usuario>();
-    List<Produto> listaProdutos = new ArrayList<Produto>();
-    List<Carrinho> listaCarrinhos = new ArrayList<Carrinho>();
-    List<Pedido> listaPedidos = new ArrayList<Pedido>();
-
-    int opcao = 0;
-
-    while (opcao != 9) {
-      System.out.println(".｡..｡..｡..｡..｡..｡..｡..｡..｡..｡..｡.");
-      System.out.println(" ");
-      System.out.println("☆*:.｡.Bem Vindo a Zonko's.｡.:*☆");
-      System.out.println("Escolha uma opção:");
-      System.out.println("1 - Cadastrar usuário");
-      System.out.println("2 - Verificar usuário");
-      System.out.println("3 - Cadastrar produto");
-      System.out.println("4 - Listar produto");
-      System.out.println("5 - Adicionar produto ao carrinho");
-      System.out.println("6 - Remover produto do carrinho");
-      System.out.println("7 - Listar carrinho");
-      System.out.println("8 - Fazer pedido");
-      System.out.println("9 - Sair");
-
-      opcao = scan.nextInt();
-
-      switch (opcao) { // cadastrar usuario e verificar
-        case 1:
-          System.out.println("Digite o nome do usuário:");
-          String nomeUsuario = scan.next();
-          System.out.println("Digite o email do usuário:");
-          String emailUsuario = scan.next();
-          System.out.println("Digite a senha do usuário:");
-          int senhaUsuario = scan.nextInt();
-          int idUsuario = listaUsuarios.size() + 1;
-
-          Usuario usuario = new Usuario(nomeUsuario, emailUsuario, senhaUsuario, idUsuario);
-          listaUsuarios.add(usuario);
-          break;
-        case 2:
-          System.out.println("Digite o email do usuário:");
-          String email = scan.next();
-          System.out.println("Digite a senha do usuário:");
-          int senha = scan.nextInt();
-
-          boolean emailVerificado = false;
-          boolean usuarioVerificado = false;
-
-          for (Usuario user : listaUsuarios) {
-            if (user.getEmail().equals(email)) {
-              emailVerificado = true;
-              if (user.getSenha() == senha) {
-                usuarioVerificado = true;
-                break;
-              }
-            }
-          }
-
-          if (emailVerificado == true && usuarioVerificado == true) {
-            System.out.println("Usuário verificado com sucesso.");
-          }
-          else {
-            System.out.println("Usuário inválido");
-          }
-      }
-      opcao = scan.nextInt();
-
-      switch (opcao) {
-        case 1:
-          Produto produto = new Produto(nomeProduto, precoProduto, qtdestoqueProduto, idProduto);
-          System.out.println("Digite o nome do produto:");
-          String nomeProduto = scan.next();
-          System.out.println("Digite o preço do produto:");
-          int precoProduto = scan.nextInt();
-          System.out.println("Digite a quantidade em estoque:");
-          int qtdestoqueProduto = scan.nextInt();
-          int idProduto = listaProdutos.size() + 1;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+              
+        List<Usuario> listaUsuarios = new ArrayList<Usuario>();
+        List<Produto> listaProdutos = new ArrayList<Produto>();
+        List<Carrinho> listaCarrinhos = new ArrayList<Carrinho>();
+        List<Pedido> listaPedidos = new ArrayList<Pedido>();
+              
+        int opcao = 0;
+              
+        while (opcao != 9) {
+            System.out.println(".｡..｡..｡..｡..｡..｡..｡..｡..｡..｡..｡.");
+            System.out.println(" ");
+            System.out.println("☆*:.｡.Bem Vindo a Zonko's.｡.:*☆");
+            System.out.println("Escolha uma opção:");
+            System.out.println("1 - Cadastrar usuário");
+            System.out.println("2 - Verificar usuário");
+            System.out.println("3 - Adicionar produto ao carrinho");
+            System.out.println("4 - Remover produto do carrinho");
+            System.out.println("5 - Fazer pedido");
+            System.out.println("6 - Sair");
+              
+            opcao = scan.nextInt();
+              
+            switch (opcao) { // cadastrar usuario e verificar
+                case 1:
+                    System.out.println("Digite o nome do usuário:");
+                    String nomeUsuario = scan.next();
+                    System.out.println("Digite o email do usuário:");
+                    String emailUsuario = scan.next();
+                    System.out.println("Digite a senha do usuário:");
+                    int senhaUsuario = scan.nextInt();
+                    int idUsuario = listaUsuarios.size() + 1;
+              
+                    Usuario usuario = new Usuario(nomeUsuario, emailUsuario, senhaUsuario, idUsuario);
+                    listaUsuarios.add(usuario);
                     break;
-
                 case 2:
-                    Carrinho carrinho = new Carrinho();
+                    System.out.println("Digite o email do usuário:");
+                    String email = scan.next();
+                    System.out.println("Digite a senha do usuário:");
+                    int senha = scan.nextInt();
+              
+                    boolean emailVerificado = false;
+                    boolean usuarioVerificado = false;
+              
+                    for (Usuario user : listaUsuarios) {
+                        if (user.getEmail().equals(email)) {
+                            emailVerificado = true;
+                            if (user.getSenha() == senha) {
+                              usuarioVerificado = true;
+                              break;
+                            }
+                          }
+                        }
+              
+                        if (emailVerificado == true && usuarioVerificado == true) {
+                          System.out.println("Usuário verificado com sucesso.");
+                        }
+                        else {
+                          System.out.println("Usuário inválido");
+                        }
 
-                    System.out.print("Digite a quantidade de produtos a ser adicionada ao carrinho: ");
-                    int qtd = scan.nextInt();
-
-                    for (int i = 0; i < qtd; i++) {
-                        System.out.print("Digite o ID do produto: ");
-                        int id = scan.nextInt();
-                        carrinho.adicionarProduto(id);
-                    }
-
-                    listaCarrinhos.add(carrinho);
-                    break;
-
-                case 3:
-                    Pedido pedido = new Pedido();
-
-                    System.out.print("Digite o ID do carrinho: ");
-                    int id = scan.nextInt();
-                    pedido.setPrecoTotal(id);
-
-                    listaPedidos.add(pedido);
-                    break;
-      }
-    }
-  }
-}
+                        case 3:
+                            System.out.println("Digite o nome do produto:");
+                            String nome = scan.next();
+                            System.out.println("Digite o preço do produto:");
+                            int preco = scan.nextInt();
+                            System.out.println("Digite o id do produto:");
+                            int id = scan.nextInt();
+                            System.out.println("Digite a quantidade em estoque:");
+                            int qtdEstoque = scan.nextInt();
+                            listaProdutos.add(new Produto(id, nome, preco, qtdEstoque));
+                            break;
+                        case 4:
+                            System.out.println("Digite o nome do produto que deseja remover:");
+                            String nomeRemover = scan.next();
+                            for (int i = 0; i < listaProdutos.size(); i++) {
+                                Produto p = listaProdutos.get(i);
+                                if (p.getNome().equals(nomeRemover)) {
+                                    listaProdutos.remove(p);
+                                    System.out.println("Produto removido com sucesso!");
+                                    break;
+                                }
+                            }
+                            break;
+                        case 5:
+                            System.out.println("Lista de produtos:");
+                            for (int i = 0; i < listaProdutos.size(); i++) {
+                              System.out.println(i + " - " + listaProdutos.get(i).getNome());
+                            }
+                            
+                            System.out.println("Selecione o produto:");
+                            int idProduto = scan.nextInt();
+                            
+                            System.out.println("Quantidade desejada:");
+                            int qtdDesejada = scan.nextInt();
+                            
+                            Produto produtoSelecionado = listaProdutos.get(idProduto);
+                        case 6:
+                            System.out.println("Saindo...");
+                            break;
+                        default:
+                            System.out.println("Opção inválida");
+                            break;
+                } // fim do switch
+            }  // while
+        } // metodo
+} // classe
